74a3771e4ded7040fc953778735ec64ee8b5e89d
Naushir Patuck <naush@raspberrypi.com>
2020-01-21 16:19:46 +0000

    media: uapi: v4l-ctrls: Add user CID base for the bcm2835-unicam driver
    Added UAPI header for the BRCM2835 Unicam device driver.
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

---
+++ include/uapi/linux/bcm2835-unicam.h
@@ -0,0 +1,29 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * bcm2835-unicam.h
+ *
+ * BCM2835 Unicam driver - user space header file.
+ *
+ * Copyright (C) 2020 - Raspberry Pi (Trading) Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __BCM2835_UNICAM_H_
+#define __BCM2835_UNICAM_H_
+
+#include <linux/v4l2-controls.h>
+
+#define V4L2_CID_USER_UNICAM_STAGGERED_WRITE		\
+				(V4L2_CID_USER_BCM2835_UNICAM_BASE + 0x01)
+#define V4L2_CID_USER_UNICAM_GROUP_HOLD			\
+				(V4L2_CID_USER_BCM2835_UNICAM_BASE + 0x02)
+
+#endif /* __BCM2835_UNICAM_H_ */
